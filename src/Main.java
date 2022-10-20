fb203c7243e82e3e23ee4a52ee12f8ee76ecab68
drewspampy22 <drewspampy22@gmail.com>
2022-10-19 21:01:19 -0400

    c

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args)
     {
-        for ( int n = 10 ; n >= 0 ; n = n-2 )
-        {
-            System.out.println( n );
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
     }
 }
+
